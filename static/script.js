9a4f35ead2bda6f85345fa13e4ba20c4bec35d51
Thiago Campos <commit@thigcampos.com>
2024-11-21 11:02:30 -0300

    improvements in script to update favicon

--- static/script.js
+++ static/script.js
@@ -1,9 +1,8 @@
 function setFavicon() {
     const favicon = document.querySelector('link[rel="icon"]');
-    console.log(favicon)
     favicon.href = (window.matchMedia('(prefers-color-scheme: dark)').matches)
-                    ? 'favicon-dark.ico'
-                    : 'favicon-light.ico';
+                    ? '/favicon-dark.ico'
+                    : '/favicon-light.ico';
 };
 
 // Call on load
